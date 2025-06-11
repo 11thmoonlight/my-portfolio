945a018b59987e0eb8d3b28f860f2287ec4dac79
aida <aidasaeedi1998@gmail.com>
2025-06-11 22:09:00 +0330

    delete unnessary files

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import Hero from "./sections/Hero.jsx";
 import ShowcaseSection from "./sections/ShowcaseSection.jsx";
 import Navbar from "./components/Navbar.jsx";
-import FeatureCards from "./sections/FeatureCards.jsx";
 import TechStack from "./sections/TechStack.jsx";
 import Contact from "./sections/Contact.jsx";
 import Footer from "./sections/Footer.jsx";
@@ -12,7 +11,6 @@ function App() {
             <Navbar/>
             <Hero/>
             <ShowcaseSection/>
-            <FeatureCards/>
             <TechStack/>
             <Contact/>
             <Footer/>
